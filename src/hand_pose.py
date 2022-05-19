22fdfe7fd42fdd26a5aab36bcdbdbc0ea067fa8d
lorenzobalzani <balzanilo@gmail.com>
2022-05-19 14:58:12 +0200

    Added max_num_hands parameter

--- src/hand_pose.py
+++ src/hand_pose.py
@@ -15,7 +15,7 @@ def draw_keypoints(video, keypoints, fps):
         frames.append(frame)
     return mpy.ImageSequenceClip(frames, fps=fps)
 
-def run_detection_hands(video, model_complexity=1, min_detection_confidence=0.5, min_tracking_confidence=0.5):
+def run_detection_hands(video, model_complexity=1, min_detection_confidence=0.5, min_tracking_confidence=0.5, max_num_hands=2):
     """Run hand gestures detection model using Google's Mediapipe
 
     Parameters:
@@ -23,16 +23,18 @@ def run_detection_hands(video, model_complexity=1, min_detection_confidence=0.5,
     model_complexity (Int): Complexity of the hand landmark model: 0 or 1. Landmark accuracy as well as inference latency generally go up with the model complexity. Default to 1.
     min_detection_confidence (Float): Minimum confidence value ([0.0, 1.0]) from the hand detection model for the detection to be considered successful. Default to 0.5.
     min_tracking_confidence (Float): Minimum confidence value ([0.0, 1.0]) from the landmark-tracking model for the hand landmarks to be considered tracked successfully, or otherwise hand detection will be invoked automatically on the next input image. 
+    max_num_hands (Int): Maximum number of hands to detect. Default to 2.
 
     Returns:
     List[List[landmark]]: A matrix of keypoints. Rows represent frames, columns represent detected 3D keypoints. 
    """
     keypoints = []
-    with mp_hands.Hands(model_complexity=model_complexity, min_detection_confidence=min_detection_confidence, min_tracking_confidence=min_tracking_confidence) as hands:
+    with mp_hands.Hands(model_complexity=model_complexity, min_detection_confidence=min_detection_confidence, min_tracking_confidence=min_tracking_confidence, max_num_hands=max_num_hands) as hands:
         for frame in video.iter_frames():
             frame_keypoints = hands.process(frame)
             if frame_keypoints.multi_hand_landmarks:
-                keypoints.append([hand_landmarks for hand_landmarks in frame_keypoints.multi_hand_landmarks])
+                keypoints.append([hand_landmarks
+                    for hand_landmarks, handedness in zip(frame_keypoints.multi_hand_landmarks, frame_keypoints.multi_handedness)])
             else:
                 keypoints.append([])
     return keypoints
