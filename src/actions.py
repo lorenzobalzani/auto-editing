2d0073108a7650ab529bfdb03f56bbe0c28f27ea
lorenzobalzani <balzanilo@gmail.com>
2022-05-23 16:59:47 +0200

    Added insert_intro action

--- src/actions.py
+++ src/actions.py
@@ -1,9 +1,10 @@
 import datetime
 
 def transform_into_timestamps(detected_gestures):
+    seconds_before_and_after_signs = datetime.timedelta(seconds=1)
+    threshold = datetime.timedelta(seconds=5)
+
     def get_cut_timestamps(detected_gestures):
-        seconds_before_and_after_signs = datetime.timedelta(seconds=1)
-        threshold = datetime.timedelta(seconds=5)
         groups = []
         for timestamp in detected_gestures:
             if not groups or timestamp - group[0] > threshold:
@@ -21,19 +22,30 @@ def transform_into_timestamps(detected_gestures):
                 timestamps.append((str(begin), str(end)))
         return timestamps
 
+    def get_insert_intro_timestamps(detected_gestures):
+        begin = min(detected_gestures) - seconds_before_and_after_signs
+        end = max(detected_gestures) + seconds_before_and_after_signs
+        timestamps = (str(begin), str(end))
+        return timestamps
+
     timestamps = {}
     for gesture_type in detected_gestures.keys():
-        if gesture_type == 'cut':  # CHANGE THIS
-            timestamps[gesture_type] = eval('get_' + gesture_type + '_timestamps')(detected_gestures[gesture_type])
+        if len(detected_gestures[gesture_type]) == 0:
+            continue
+        timestamps[gesture_type] = eval('get_' + gesture_type + '_timestamps')(detected_gestures[gesture_type])
     return timestamps
 
-def operate_action(action, video, timestamps):
-    def cut(video, timestamps):
+def operate_action(action, video, timestamps, extra_parameters={}):
+    def cut(video, timestamps, extra_parameters):
         for cut in timestamps:
             video = video.cutout(cut[0], cut[1])
         return video
 
-    def insert_intro(video, timestamps):
+    def insert_intro(video, timestamps, extra_parameters):
+        if not 'intro_video' in extra_parameters.keys():
+            return video
+        for cut in timestamps:
+            video = video.cutout(cut[0], cut[1])
         return video
 
-    return eval(action)(video, timestamps)
+    return eval(action)(video, timestamps, extra_parameters)
