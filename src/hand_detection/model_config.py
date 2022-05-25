79061a3b63f9ab1ee8213afe08bb117fecb73f79
lorenzobalzani <balzanilo@gmail.com>
2022-05-25 15:36:08 +0200

    Update model_config.py
    Changed batch_size and num_classes hyperparameters.

--- src/hand_detection/model_config.py
+++ src/hand_detection/model_config.py
@@ -1,10 +1,10 @@
 # Model training
-num_classes = 6
+num_classes = 2
 random_state = 42
 num_features = 42
 train_size = 0.75
 epochs = 1000
-batch_size = 64
+batch_size = 16
 es_patience = 20
 
 # Model inference
